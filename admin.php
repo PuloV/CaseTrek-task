e830c5aedd907a3bca8e75fb08abf2b597855712
PuloV <yordanpulov@gmail.com>
2014-04-27 19:16:46 +0300

    Title changed

--- admin.php
+++ admin.php
@@ -27,11 +27,10 @@ $features = $f->all();
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>jQuery UI Sortable - Default functionality</title>
+<title>Admin View</title>
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
 <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
 <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-<link rel="stylesheet" href="/resources/demos/style.css">
 <style>
 #sortable { list-style-type: none; margin: 0; padding: 0; width: 60%; }
 #sortable li { margin: 0 3px 3px 3px; padding: 0.4em; padding-left: 1.5em; font-size: 1.4em; height: 18px; }
